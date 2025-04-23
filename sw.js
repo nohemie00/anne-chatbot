c6113a95a52b0962bb5c6161ab2d872bcd516512
nohemie00 <nohemie2525@gmail.com>
2025-04-23 10:54:08 +0900

    sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'anne-chatbot-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll([
+                    '/',
+                    '/assets/anne.jpg'
+                ]);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
